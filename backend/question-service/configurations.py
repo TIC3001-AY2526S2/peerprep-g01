3bdb5110fb0f1b7b428d688cd8285bc220e7bf39
DS <>
2026-03-07 23:07:19 +0800

    Moved a few things
    Gonna try to move the project to raspberry pi to address proton vpn issue

--- backend/question-service/configurations.py
+++ backend/question-service/configurations.py
@@ -2,12 +2,20 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
+import requests
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Get the MongoDB URI from the environment variable
 uri = os.getenv("DB_CLOUD_URI")
+
+print (f'[*] URI - {uri}')
+
+# ip_request = requests.get('https://ipwho.is')
+# data = ip_request.json()
+# print(f'[+] IP Address - {data.get('ip')}')
+
 if not uri:
     raise ValueError("DB_CLOUD_URI not set in .env file")
 
